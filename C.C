6de9639c4f9632214348b30bfc545abde382c0ed
Patrycja <you@example.com>
2023-10-09 19:14:43 +0200

    siała baba mak nie wiedziała jak

---
+++ C.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main()
+{
+    int N, 
+    printf("Podaj wartosc N\n");
+    scanf("%d", &N);
+    printf("twoja wartoœæ to:................%d.........../n Brawo wyrales",&N);
+ 
+}
+
+
+
+
